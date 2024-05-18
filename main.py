3074af4b96369138c6fbaa6ce2f17ebc6c8918c9
JeffersonAhimar <jeffersonahimar@outlook.com>
2024-05-18 08:54:20 -0500

    modify logs

--- main.py
+++ main.py
@@ -16,14 +16,11 @@ load_dotenv('.env')
 URL = os.getenv('URL_HELP_DESK')
 USERNAME = os.getenv('HD_USERNAME')
 PASSWORD = os.getenv('HD_PASSWORD')
-print(USERNAME)
-print(PASSWORD)
-
 
 driver = webdriver.Chrome()
 driver.get(URL)
 
-driverWait = WebDriverWait(driver, 5)
+driverWait = WebDriverWait(driver, 2)
 
 
 def login():
@@ -56,19 +53,22 @@ def openSubjectByTicketId(ticketId):
     textHref = f"//a[@href='WorkOrder.do?woMode=viewWO&woID={ticketId}&&fromListView=true&fromAdvSearch=true']"
     try:
         subjectBtn = driverWait.until(EC.presence_of_element_located((By.XPATH, textHref)))
+        print("✔️ Ticket found!")
         subjectBtn.click()
     except TimeoutException:
         goToNextPage(25,2)
         try:
             subjectBtn = driverWait.until(EC.presence_of_element_located((By.XPATH, textHref)))
+            print("✔️ Ticket found!")
             subjectBtn.click()
         except TimeoutException:
             goToNextPage(50,3)
             try:
                 subjectBtn = driverWait.until(EC.presence_of_element_located((By.XPATH, textHref)))
+                print("✔️ Ticket found!")
                 subjectBtn.click()
             except TimeoutException:
-                print("No se encontró en la página 2 ni en la 3")
+                print("❌ Ticket wasn't found!")
 
 
 def goToNextPage(offset,page):
@@ -76,34 +76,58 @@ def goToNextPage(offset,page):
     showRange('RequestsView', '{offset+1}','{page}')
     """
     driver.execute_script(txt_script)
+    print(f"...Searching on next page -> {page}")
 
 
 def openEditForm():
     editBtn = driverWait.until(EC.presence_of_element_located((By.ID, "Req_Det_Edit")))
+    print(f"Opening form to edit status")
     editBtn.click()
 
 def run():
+
+    changedTickets=[]
+    unchangedTickets=[]
+    ticketsList = [
+        111812,112624
+    ]
+
     try:
         login()
 
-        ticketsList = [
-            112404,112476
-        ]
+        print("*************************************")
+        print(f"Requested Tickets: {ticketsList}")
+        print(f"# of requested Tickets: {len(ticketsList)}")
+        print("*************************************")
 
         for ticket in ticketsList:
+            print("-------------------------------------")
             print(f"Searching ticket # {ticket}")
             openRequest()
             openAdvanceSearch()
-            openSubjectByTicketId(ticket)
-            openEditForm()
-            print(f"Editing status of ticket # {ticket} \n")
+            try:
+                openSubjectByTicketId(ticket)
+                openEditForm()
+                print(f"Executing script")
+                # driver.execute_script(scriptChangeTicketStatus())
+                changedTickets.append(ticket)
+            except:
+                print("Ticket status wasn't changed")
+                unchangedTickets.append(ticket)
+            print("------------------------------------- \n")
             time.sleep(1) # seconds
-            # driver.execute_script(scriptChangeTicketStatus())
 
     except:
         print("Unknown error")
     finally:
-        print(f"Tickets changed: {ticketsList}")
+        print("*************************************")
+        print(f"Changed Tickets: {changedTickets}")
+        print(f"# of changed Tickets: {len(changedTickets)}")
+        print("*************************************")
+        print("*************************************")
+        print(f"Unchanged tickets: {unchangedTickets}")
+        print(f"# of unchanged Tickets: {len(unchangedTickets)}")
+        print("*************************************")
         time.sleep(5) # seconds
         driver.quit()
 
